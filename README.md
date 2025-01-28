1741390e29ee1290853f2e37bbb437234ae0b1c7
YourName <davidcyril209@gmail.com>
2025-01-28 11:27:14 +0100

    Queen_Anita_V4

---
+++ README.md
@@ -0,0 +1,16 @@
+ ## QUEEN_ANITA-V4 Deployment Methods
+
+### 1. FORK THIS REPO
+
+<a href='https://github.com/DeeCeeXxx/Queen_Anita-V4/fork' target="_blank"><img alt='Fork repo' src='https://img.shields.io/badge/Fork This Repo-black?style=for-the-badge&logo=git&logoColor=white'/></a>
+
+### 2. GET SESSION ID HERE
+
+### SERVER 1 
+ 
+<a href="https://anita-v4-pairing-wumi.onrender.com"><img src="https://img.shields.io/badge/SESSION_ID-blue" alt="Click Here to Get Pair-Code" width="110"></a>   
+
+
+
+#### ```DAVID CYRIL PROFILE VIEWS 🧚```
+![Visitor Count](https://profile-counter.glitch.me/DeeCeeXxx/count.svg)
---
+++ config.js
@@ -0,0 +1,75 @@
+require('dotenv').config(); 
+
+global.owner = process.env.OWNER_NUMBER.split(','); 
+global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
+global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
+global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
+global.autoTyping = process.env.AUTO_TYPING === 'true'; 
+global.autoRecord = process.env.AUTO_RECORD === 'true';
+global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
+global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
+
+global.LEVELUP = process.env.LEVELUP === 'true';
+
+global.ANTIVIEWONCE = process.env.ANTIVIEWONCE === 'true';
+
+global.roles = {
+    1: "Novice",
+    3: "Learner",
+    5: "Apprentice",
+    8: "Magician",
+    10: "Wizard",
+    15: "Sorcerer",
+    20: "Grandmaster",
+    25: "Legend",
+};
+
+global.sudoUsers = process.env.SUDO_USERS.split(',').map(user => `${user}@s.whatsapp.net`); 
+
+global.public = process.env.PUBLIC === 'true';
+
+global.ANTIDELETE = process.env.ANTIDELETE === 'true';
+global.unavailable = process.env.UNAVAILABLE === 'true';
+global.available = process.env.AVAILABLE === 'true';
+global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'true';
+global.chatbot = process.env.CHATBOT === 'true';
+global.autoreact = process.env.AUTO_REACT === 'true';
+
+global.WELCOME = process.env.WELCOME === 'true';
+
+global.prefix = process.env.PREFIX || '.';
+
+global.autobio = process.env.AUTO_BIO === 'true';
+
+global.ANTICALL = process.env.ANTICALL === 'true';
+
+global.antilink = process.env.ANTILINK === 'true';
+global.antilinkkick = process.env.ANTILINK_KICK === 'true';
+global.antilinkwarn = process.env.ANTILINK_WARN === 'true';
+
+global.mess = {
+    success: process.env.MESSAGE_SUCCESS || '𝙳𝚘𝚗𝚎',
+    admin: process.env.MESSAGE_ADMIN || '_*❗This Command Can Only Be Used By Group Admins !*_',
+    botAdmin: process.env.MESSAGE_BOT_ADMIN || '_*❗This Command Can Only Be Used When Bot Becomes Group Admin !*_',
+    OnlyOwner: process.env.MESSAGE_OWNER || '_*❗This Command Can Only Be Used By My Owner !*_',
+    OnlyGrup: process.env.MESSAGE_GROUP || '_*❗This Command Can Only Be Used In Group Chat !*_',
+    private: process.env.MESSAGE_PRIVATE || '_*❗This Command Can Only Be Used In Private Chat !*_',
+    wait: process.env.MESSAGE_WAIT || '_*Please Wait*_',
+    notregist: process.env.MESSAGE_NOT_REGIST || '_*You are not registered in the Bot Database. Please register first*_',
+    premium: process.env.MESSAGE_PREMIUM || '_*Premium only" Want Premium? Chat Owner*_',
+    endLimit: process.env.MESSAGE_END_LIMIT || '_*Your Daily Limit Has Been Used Up, The Limit Will Be Reset Every 00:00 AM_*.',
+};
+
+
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
